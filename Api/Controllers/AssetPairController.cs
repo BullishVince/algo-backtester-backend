035fba079ef09280270aae3dac13d9a749acbbfc
BullishVince <vincent@bullishlife.com>
2022-07-12 02:14:46 +0200

    add endpoint for fetching assetpair data from file

---
+++ Api/Controllers/AssetPairController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using AlgoBacktesterBackend.Api.Services;
+
+namespace AlgoBacktesterBackend.Api.Controllers;
+[ApiController]
+[Route("[controller]/[action]")]
+public class AssetPairController : ControllerBase
+{
+
+    private readonly ILogger<AssetPairController> _logger;
+    private readonly IAssetPairService _assetPairService;
+
+    public AssetPairController(
+        ILogger<AssetPairController> logger, 
+        IAssetPairService assetPairService
+        )
+    {
+        _logger = logger;
+        _assetPairService = assetPairService;
+    } 
+
+    [HttpGet]
+    public async Task<IActionResult> GetAssetPairFromFile(string fileName)
+    {
+        var result = await _assetPairService.GetAssetPairDataFromFile(fileName);
+        return Ok(result);
+    }         
+}

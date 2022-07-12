70bfc0d4ca382f1a38812cdfe7f696de2ad1f565
BullishVince <vincent@bullishlife.com>
2022-07-12 14:30:44 +0200

    add StringExtension for MultiLineInsert + fix fetching of data from HistData.com files

--- Domain/Repository/AssetPairRepository.cs
+++ Domain/Repository/AssetPairRepository.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using AlgoBacktesterBackend.Domain.Models;
+using AlgoBacktesterBackend.Extensions;
 
 namespace AlgoBacktesterBackend.Domain.Repository;
 public interface IAssetPairRepository {
@@ -9,19 +11,22 @@ public class AssetPairRepository : IAssetPairRepository {
     public AssetPair GetHistoricalAssetPairData(string ticker, DateTime? startDate, DateTime? endDate) {
         return new AssetPair(ticker);
     }
-
     public async Task<AssetPair> GetHistoricalAssetPairDataFromFile(string ticker, string fileName) {
         var assetPair = new AssetPair(ticker);
         foreach (string line in await File.ReadAllLinesAsync(fileName)) {
             var data = line.Split(';', 6, StringSplitOptions.RemoveEmptyEntries);
+
+            //Fix the formatting since the DateTime field in the csv's from HistData.com is invalid
+            data[0] = data[0].MultiInsert("-", 3, 5).Replace(" ", "T").MultiInsert(":", 12, 14);
+
             try {
                 var dataPoint = new DataPoint(
-                    DateTime.Parse(data[0]), 
-                    decimal.Parse(data[1]), 
-                    decimal.Parse(data[2]), 
-                    decimal.Parse(data[3]), 
-                    decimal.Parse(data[4]), 
-                    decimal.Parse(data[5]));
+                    DateTime.Parse(data[0], CultureInfo.InvariantCulture), 
+                    decimal.Parse(data[1], CultureInfo.InvariantCulture), 
+                    decimal.Parse(data[2], CultureInfo.InvariantCulture), 
+                    decimal.Parse(data[3], CultureInfo.InvariantCulture), 
+                    decimal.Parse(data[4], CultureInfo.InvariantCulture), 
+                    decimal.Parse(data[5], CultureInfo.InvariantCulture));
                 assetPair.AddDataPoint(dataPoint);
             } catch (Exception e) {
                 Console.WriteLine(e);
